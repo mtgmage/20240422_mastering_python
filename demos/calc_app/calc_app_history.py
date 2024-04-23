30b63a271089d71b317551aea9d271355e5a7040
mtgmage <32280943+mtgmage@users.noreply.github.com>
2024-04-23 18:13:46 +0000

    Commit updates to calc program

--- demos/calc_app/calc_app_history.py
+++ demos/calc_app/calc_app_history.py
@@ -1,50 +1,35 @@
+from typing import Callable
+
+
+la_math_operations: dict[str, Callable[[float, float], float]] = {
+    "add": lambda ln_num1, ln_num2: ln_num1 + ln_num2,
+    "subtract": lambda ln_num1, ln_num2: ln_num1 - ln_num2,
+    "multiply": lambda ln_num1, ln_num2: ln_num1 * ln_num2,
+    "divide": lambda ln_num1, ln_num2: ln_num1 / ln_num2,
+    }
+
 def store_opperation(pc_command: str, pn_delta: float, pa_opperations: dict) -> None:
     if len(pa_opperations) > 0:
         ln_id = max(pa_opperations.keys()) + 1
     else:
         ln_id = 1
 
-    match pc_command:
-        case "+":
-            pc_command = "add"
-        case "-":
-            pc_command = "subtract"
-        case "*":
-            pc_command = "multiply"
-        case "/":
-            pc_command = "divide"
-
     pa_opperations[ln_id] = {"command": pc_command, "delta_value": pn_delta}
 
 
-def get_delta() -> float | None:
+def get_delta(pc_command: str) -> float | None:
     lc_delta = input("Enter the delta value: ")
 
     try:
         ln_delta = float(lc_delta)
+        if pc_command == "divide" and ln_delta == 0:
+            print("Cannot divide by 0!")
+            ln_delta = None
     except:
         print("Invalid delta value provided!")
         ln_delta = None
 
     return ln_delta
-
-
-def update_value(pc_command: str, pn_value: float, pn_delta: float) -> float | None:
-    ln_value = None
-    match pc_command:
-        case "add" | "+":
-            ln_value = pn_value + pn_delta
-        case "subtract" | "-":
-            ln_value = pn_value - pn_delta
-        case "multiply" | "*":
-            ln_value = pn_value * pn_delta
-        case "divide" | "/":
-            if pn_delta == 0:
-                print("Cannot divide by 0!")
-            else:
-                ln_value = pn_value / pn_delta
-
-    return ln_value
 
 
 def remove_command(pa_opperations: dict) -> dict:
@@ -64,65 +49,73 @@ def remove_command(pa_opperations: dict) -> dict:
     return pa_opperations
 
 
+def current_value(pa_opperations: dict) -> float:
+    ln_value = 0
+    for ln_id in pa_opperations.keys():
+        ln_value = la_math_operations[pa_opperations[ln_id]["command"]](ln_value, pa_opperations[ln_id]["delta_value"])
+
+    return ln_value
+
+
 def main() -> None:
-    ln_value: float = 0.0
     la_opperations: dict = {}
+    la_hotkeys = {
+        "+": "add",
+        "-": "subtract",
+        "*": "multiply",
+        "/": "divide",
+        "c": "clear",
+        "h": "history",
+        "r": "remove",
+        "x": "exit",
+        }
 
     for ln_loop in range(0, 999):
         print(" ")
         print("****************************************")
-        print("The current value is: " + str(ln_value))
+        print("The current value is: " + str(current_value(la_opperations)))
         print("****************************************")
         print(" ")
         print("Here are the valid commands:")
-        print("add (+)")
-        print("subtract (-)")
-        print("multiply (*)")
-        print("divide (/)")
+        print("add (+), subtract (-), multiply (*), divide (/)")
         print("clear (c) - to reset current value to zero")
         print("history (h) - to show the previous commands")
         print("remove (r) - to remove a command from the history")
         print("exit (x) - to terminate")
         lc_command = input("Enter a command: ")
         lc_command = lc_command.lower()
+        lc_command = la_hotkeys.get(lc_command, lc_command)
 
-        match lc_command:
-            case "exit" | "x":
-                print(" ")
-                print("Bye :)")
-                print(" ")
-                break
+        if lc_command in la_math_operations:
+            ln_delta = get_delta(lc_command)
+            if (ln_delta == None):
+                continue
 
-            case "add" | "+" | "subtract" | "-" | "multiply" | "*" | "divide" | "/":
-                ln_delta = get_delta()
-                if ln_delta == None:
-                    continue
+            store_opperation(lc_command, ln_delta, la_opperations)
 
-                ln_value = update_value(lc_command, ln_value, ln_delta)
-                if ln_value == None:
-                    continue
+        elif lc_command == "clear":
+            la_opperations = {}
 
-                store_opperation(lc_command, ln_delta, la_opperations)
+        elif lc_command == "history":
+            print(" ")
+            print("Opperation history:")
+            if len(la_opperations) > 0:
+                for ln_id in la_opperations.keys():
+                    print(str(ln_id) + " " + la_opperations[ln_id]["command"] + " " + str(la_opperations[ln_id]["delta_value"]))
+            else:
+                print("None")
 
-            case "clear" | "c":
-                ln_value = 0
-                la_opperations = {}
+        elif lc_command == "remove":
+            la_opperations = remove_command(la_opperations)
 
-            case "history" | "h":
-                print(" ")
-                print("Opperation history:")
-                if len(la_opperations) > 0:
-                    for ln_id in la_opperations.keys():
-                        print(str(ln_id) + " " + la_opperations[ln_id]["command"] + " " + str(la_opperations[ln_id]["delta_value"]))
-                else:
-                    print("None")
+        elif lc_command == "exit":
+            print(" ")
+            print("Bye :)")
+            print(" ")
+            break
 
-            case "remove" | "r":
-                la_opperations = remove_command(la_opperations)
-
-
-            case _:
-                print("Invalid command provided!")
+        else:
+            print("Invalid command provided!")
 
 
 if __name__ == "__main__":
